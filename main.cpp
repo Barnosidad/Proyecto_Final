f257ace2736330c978288a129e1b7907ca6add03
Diego Gonzalez <gh098013@gmail.com>
2023-11-28 18:26:14 -0600

    Pila par
    Añadi mas funciones y la pila par se puede crear.

--- main.cpp
+++ main.cpp
@@ -9,11 +9,14 @@ struct Nodo
 };
 
 void print_menu(int *ptr_value);
+void print_menu_pilas(int *ptr_value);
 void pedirDatosV(int *ptr_value);
 void agregar_pila(Nodo *&pila, int n);
 bool salirC(char opc);
 void eliminarNodos(Nodo *&pila);
 bool pila_existente(Nodo *&pila);
+void recorrerPila(Nodo *&pila);
+void pila_par(Nodo *&pilaS, Nodo *&pilaO);
 
 int main()
 {
@@ -27,7 +30,7 @@ int main()
     // Variables
     int n = 0, *ptr_n = &n;
     char opc;
-    bool salir = false;
+    bool salir = false, salir_v = false;;
     do
     {
         print_menu(ptr_n);
@@ -46,7 +49,32 @@ int main()
             case 2:
                 break;
             case 3:
-                break;
+                if(pila_existente(pila_original))
+                {
+                    do
+                    {
+                        print_menu_pilas(ptr_n);
+                        switch (*ptr_n)
+                        {
+                            case 1:
+                                recorrerPila(pila_original);
+                                break;
+                            case 3:
+                                pila_par(pila_original,pila_pares);
+                                recorrerPila(pila_pares);
+                                break;
+                            default:
+                                salir_v = true;
+                                break;
+                        }
+                    } while (!salir_v);
+                    break;
+                }
+                else
+                {
+                    std::cout << "Y la pila papito?" << std::endl;
+                    break;
+                }
             case 4:
                 if(!pila_existente(pila_original))
                 {
@@ -56,8 +84,9 @@ int main()
                 else
                 {
                     eliminarNodos(pila_original);
+                    eliminarNodos(pila_pares);
+                    break;
                 }
-                break;
             case 5:
                 salir = true;
                 break;
@@ -118,4 +147,46 @@ void eliminarNodos(Nodo *&pila)
 bool pila_existente(Nodo *&pila)
 {
     return (pila == NULL)? false : true;
+}
+
+void recorrerPila(Nodo *&pila)
+{
+    Nodo *aux = pila;
+    int i = 0;
+    do
+    {
+        std::cout<< i << " .[" << aux->dato << "].\n" << std::endl;
+        aux = aux->siguiente;
+        i++;
+    }while(aux!=NULL);
+}
+
+void print_menu_pilas(int *ptr_value)
+{
+    std::cout << "Menu pilas" << std::endl;
+    std::cout << "1. Pila original" << std::endl;
+    std::cout << "2. Pila sin numeros repetidos" << std::endl;
+    std::cout << "3. Pila numeros pares" << std::endl;
+    std::cout << "4. Pila numeros impares" << std::endl;
+    std::cout << "5. Pila numeros positivos" << std::endl;
+    std::cout << "6. Pila numeros negativos" << std::endl;
+    std::cout << "7. Regresar al menu principal" << std::endl;
+    std::cout << "Ingresa una opcion" << std::endl;
+    pedirDatosV(ptr_value);
+}
+
+void pila_par(Nodo *&pilaS, Nodo *&pilaO)
+{
+    Nodo *aux = pilaS;
+    do
+    {
+        if(aux->dato % 2 == 0)
+        {
+            Nodo *nuevo_nodo = new Nodo();
+            nuevo_nodo->dato = aux->dato;
+            nuevo_nodo->siguiente = pilaO;
+            pilaO = nuevo_nodo;
+        }
+        aux = aux->siguiente;
+    }while(aux!=NULL);
 }
